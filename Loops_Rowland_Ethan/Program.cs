cfaeab9c7ffebd14da716f911d2a91cc661aba68
generalrowland <ethandale0916@gmail.com>
2023-04-04 13:52:04 -0500

    3

--- Loops_Rowland_Ethan/Program.cs
+++ Loops_Rowland_Ethan/Program.cs
@@ -6,7 +6,49 @@ namespace Loops_Rowland_Ethan
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Why do you want to make games?");
+            string response = Console.ReadLine();
+
+            Console.WriteLine($"The sentence, \"{response}\" has {CountNumberOfSpaces(response)} spaces in it.");
+
+            Console.WriteLine("PLease enter a whole number!");
+            string response2 = Console.ReadLine();
+
+            Console.WriteLine("The sum of the individual digits of {response2} is {SumOfDigits(response2)}!");
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sentence">The sentence to count number of spaces in</param>
+        /// <returns>Number of spaces in the sentence</returns>
+        static int CountNumberOfSpaces(string sentence)
+        {
+            int numberOfSpaces = 0;
+            foreach(char character in sentence)
+            {
+                if(character == ' ')
+                    numberOfSpaces++;
+            }
+
+            return numberOfSpaces;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        static int SumOfDigits(string number)
+        {
+            int sum = 0;
+            foreach(char digit in number)
+            {
+                sum += (int)Char.GetNumericValue(digit);
+            }
+
+            return sum;
+        }
+        
     }
 }
